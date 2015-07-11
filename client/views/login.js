1c9fcaa55ce8e4bbe6684591413ea4c4277022de
Benny Zlotnik <uber442@gmail.com>
2015-07-11 17:08:18 +0300

    Add login functionality

--- client/views/login.js
+++ client/views/login.js
@@ -1,3 +1,27 @@
-Template.login.events({
+errors = new ReactiveArray([]);
 
-})l
+Template.login.helpers({
+  errorList: function() {
+    return errors.list();
+  }
+});
+
+Template.login.events({
+  '.signin submit': function(e) {
+    e.preventDefault();
+    var user = {
+      email: $('#inputEmail').val(),
+      password: $('#inputPassword').val()
+    };
+
+    Accounts.loginWithPassword(user.email, user.password, function(err){
+      if (err) {
+        errors.push({message: err.reason});
+      } else {
+        Router.go('projects', {
+          _id: Meteor.userId()
+        });
+      }
+    });
+  }
+});
