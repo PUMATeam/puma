0324b41bd9268797259232cf52858d55a5c422e0
Benny Zlotnik <uber442@gmail.com>
2015-07-11 17:44:19 +0300

    Fix the entire thing error displaying, make it only in the scope of the template and drop ReactiveArray since it isn't really helping

--- client/views/login.js
+++ client/views/login.js
@@ -1,22 +1,32 @@
-errors = new ReactiveArray([]);
+Template.login.onCreated(function() {
+  this.errors = new ReactiveVar();
+  this.errors.set([])
+});
 
 Template.login.helpers({
+  errors: function() {
+    return this.errors;
+  },
+
   errorList: function() {
-    return errors.list();
+    return Template.instance().errors.get();
   }
 });
 
 Template.login.events({
-  '.signin submit': function(e) {
+  'submit .form-signin': function(e, template) {
     e.preventDefault();
     var user = {
       email: $('#inputEmail').val(),
       password: $('#inputPassword').val()
     };
 
-    Accounts.loginWithPassword(user.email, user.password, function(err){
+    Meteor.loginWithPassword(user.email, user.password, function(err){
       if (err) {
+        console.log(err.reason);
+        var errors = [];
         errors.push({message: err.reason});
+        template.errors.set(errors);
       } else {
         Router.go('projects', {
           _id: Meteor.userId()
