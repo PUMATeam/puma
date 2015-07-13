e993c85e5475253a169d004eab0caec58b612810
Benny Zlotnik <uber442@gmail.com>
2015-07-13 23:29:12 +0300

    Add form testing

---
+++ tests/jasmine/client/unit/forms.js
@@ -0,0 +1,21 @@
+Template.stub('register');
+
+describe("forms", function() {
+  describe("Test form validating method", function() {
+    it("return that form in data is not valid when all fields are empty", function() {
+      var user = {
+        email: '',
+        userName: '',
+        profile: {
+          userName: '',
+          firstName: '',
+          lastName: ''
+        }
+      };
+
+      var temp =
+
+      expect($('a.btn').length).toEqual(2);
+    });
+  });
+});
