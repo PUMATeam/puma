559a8da6452096d793f434592cf2aa2ed1470046
Benny Zlotnik <uber442@gmail.com>
2015-07-16 23:42:23 +0300

    Creating projects is now possible. Also, it now shows the user name in the table and not the id #6

--- client/views/projects.js
+++ client/views/projects.js
@@ -1,12 +1,24 @@
 Template.projects.helpers({
   projects: function() {
-    return Projects.find({ owner: Meteor.userId() });
+    return Projects.find({ 'owner._id': Meteor.userId() });
   },
 
   projectsEmpty: function() {
-    return Projects.find({ owner: Meteor.userId() }).count() == 0;
+    return Projects.find({ 'owner._id': Meteor.userId() }).count() == 0;
   }
 });
 
 Template.projects.events({
+  'submit #new-project': function(e, template) {
+    e.preventDefault();
+    var project = {
+      name: $('#project-name').val(),
+      description: $('#project-description').val(),
+      owner: { _id: Meteor.userId(),
+               userName: Meteor.user().profile.userName
+             }
+    };
+
+    Meteor.call('addProject', project);
+  }
 });
