3ef0027d29305bde68726076d1f6b258aef63429
Benny Zlotnik <uber442@gmail.com>
2015-07-16 14:19:09 +0300

    Make New Project page a modal

--- client/views/projects.js
+++ client/views/projects.js
@@ -7,3 +7,11 @@ Template.projects.helpers({
     return Projects.find({ owner: Meteor.userId() }).count() == 0;
   }
 });
+
+Template.projects.events({
+  // "click #create-project-button": function(e, template) {
+  //   e.preventDefault();
+  //   console.log("clicked");
+  //   $("#create-project").modal("show");
+  // }
+});
