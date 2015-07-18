5b3836cc32625d5b88c8181024c092afa5fdd387
Benny Zlotnik <uber442@gmail.com>
2015-07-18 22:33:36 +0300

    Remove collections directory

---
+++ client/templates/new_project.js
@@ -0,0 +1,7 @@
+<div class="new-project-div">
+  <form class="form-horizontal" id="new-project">
+    <input id="project-name" type="text" class="form-control form-group" placeholder="Project name">
+    <textarea id="project-description" class="form-control form-group" rows="2" placeholder="Description"></textarea>
+    <input class="btn-lg btn-primary btn-block form-group" type="submit" value="Create" />
+  </form>
+</div>
