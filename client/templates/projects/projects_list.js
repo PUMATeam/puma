f64889c9b78f5840e3c4693db6a39544b619d983
Benny Zlotnik <uber442@gmail.com>
2015-07-19 00:22:02 +0300

    Show projects from the database instead of a variable

--- client/templates/projects/projects_list.js
+++ client/templates/projects/projects_list.js
@@ -1,23 +1,5 @@
-var projectsFixture = [{
-    name: 'Project 1',
-    description: 'This the first project, taken right out of a fixture',
-    createdAt: new Date(),
-    owner: {
-      userId: '1',
-      username: 'benny'
-    }
-  },
-  {
-    name: 'Project 2',
-    description: 'This the first project, taken right out of a fixture',
-    createdAt: new Date(),
-    owner: {
-      userId: '1',
-      username: 'benny'
-    }
-  }
-];
-
 Template.projectsList.helpers({
-  projects: projectsFixture
+  projects: function() {
+    return Projects.find();
+  }
 });
