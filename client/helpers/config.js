9c44e0e2b1a473a96de33fb8d40bba8919c9906c
Benny Zlotnik <uber442@gmail.com>
2015-07-18 21:22:21 +0300

    Add Accounts-ui config file to add the fields of first name and last name, and use email to signin

---
+++ client/helpers/config.js
@@ -0,0 +1,13 @@
+Accounts.ui.config({
+  extraSignupFields:[{
+    fieldName: 'firstName',
+    fieldLabel: 'First name',
+    inputType: 'text',
+    visible: true
+  }, {
+    fieldName: 'lastName',
+    fieldLabel: 'Last name',
+    inputType: 'text',
+    visible: true  }],
+    passwordSignupFields: 'USERNAME_ONLY'
+})
