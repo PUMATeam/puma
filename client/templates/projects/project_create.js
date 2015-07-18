7f74eb4a35ac082713e32ce5371b1d62f35b51c1
Benny Zlotnik <uber442@gmail.com>
2015-07-19 00:12:24 +0300

    It is now possible to add projects

--- client/templates/projects/project_create.js
+++ client/templates/projects/project_create.js
@@ -1,13 +1,18 @@
 Template.projectCreate.events({
   'submit #new-project-form': function(e) {
     e.preventDefault();
-
     var project = {
       name: $(e.target).find('#project-name').val(),
       description: $(e.target).find('#project-description').val()
     }
 
-    project._id = Projects.insert(project);
-    Router.go('projectPage', project);
+    Meteor.call('insertProject', project, function(err, result) {
+      if (err) {
+        //TODO display errors in a more sensible manner
+        return alert(err.reason);
+      }
+
+      Router.go('projectPage', { _id: result._id });
+    });
   }
 });
