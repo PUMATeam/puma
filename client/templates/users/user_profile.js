8547386d89d3474bc06566886236272ff03b0022
Benny Zlotnik <uber442@gmail.com>
2015-07-21 19:56:02 +0300

    Add gravatar support

--- client/templates/users/user_profile.js
+++ client/templates/users/user_profile.js
@@ -1,5 +1,10 @@
 Template.userProfile.helpers({
   fullName: function() {
     return this.profile.firstName + ' ' + this.profile.lastName;
+  },
+
+  gravatarURL: function() {
+    var hash = Gravatar.hash(this.emails[0].address);
+    return Gravatar.imageUrl(hash);
   }
 });
