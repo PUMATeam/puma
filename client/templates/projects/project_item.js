f19722e7d14b1de3594f3bdfb42e4509a8606c40
Benny Zlotnik <uber442@gmail.com>
2015-07-25 20:13:26 +0300

    Add click action to project item template when clicking Join

--- client/templates/projects/project_item.js
+++ client/templates/projects/project_item.js
@@ -20,3 +20,29 @@ Template.projectItem.helpers({
     return projectOwnerOrMember.count() === 0 && Meteor.userId();
   }
 });
+
+Template.projectItem.events({
+  'click .join-button': function(e) {
+    e.preventDefault();
+    var request = {
+      user: {
+        userId: Meteor.userId(),
+        username: Meteor.user().username
+      },
+      project: {
+        name: this.name,
+        projectId: this._id,
+        ownerId: this.owner.userId
+      }
+    };
+
+    Meteor.call('addRequest', request, function (err, result) {
+      if (err) {
+
+      } else {
+        Router.go('projectPage', this._id);
+      }
+    });
+
+  }
+});
