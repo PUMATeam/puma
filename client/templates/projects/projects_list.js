ee6fb334fa49cbf425bb228152f4e574038f44f6
Benny Zlotnik <uber442@gmail.com>
2015-07-25 14:02:56 +0300

    Add canUserJoin helper method

--- client/templates/projects/projects_list.js
+++ client/templates/projects/projects_list.js
@@ -1,5 +1,10 @@
 Template.projectsList.helpers({
   projects: function() {
     return Projects.find();
+  },
+  canJoinProject:  function(userId, projectId) {
+    var projectOwnerOrMember = Projects.find({ $or: [{'owner.userId': userId, _id: projectId}, { members: { $elemMatch: { userId: userId } }] });
+
+    return projectOwnerOrMember.count() === 0;
   }
 });
