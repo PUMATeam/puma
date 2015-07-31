38d2634dd1389c0cdf9dc46d048823d682f9626f
Adam Gutglick <adamgsal@gmail.com>
2015-08-01 00:39:35 +0300

    Added routing to '/' on login or logout.

--- client/templates/helpers.js
+++ client/templates/helpers.js
@@ -1,3 +1,11 @@
 Template.registerHelper('isEmpty', function(collection) {
   return collection.count() == 0;
 });
+
+Template._loginButtons.events({
+	'click #login-buttons-logout': function (ev) {
+		if (Meteor.isClient) {
+			Router.go('/');
+		}
+	}
+});
