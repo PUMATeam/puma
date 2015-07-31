817a26e7ffce6e2694def4be3d6854d7526b514e
Adam Gutglick <adamgsal@gmail.com>
2015-08-01 00:41:35 +0300

    When logging in or out - the user will be routed to '/'.

--- client/templates/helpers.js
+++ client/templates/helpers.js
@@ -2,6 +2,7 @@ Template.registerHelper('isEmpty', function(collection) {
   return collection.count() == 0;
 });
 
+// Just a cosmetic change - when logging out, route to '/'.
 Template._loginButtons.events({
 	'click #login-buttons-logout': function (ev) {
 		if (Meteor.isClient) {
