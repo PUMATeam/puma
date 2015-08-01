c811f5240ad6b7cf4ab8f3c06d1b18f5fb9b6463
Benny Zlotnik <uber442@gmail.com>
2015-08-01 18:21:28 +0300

    Add click event to accept request

--- client/templates/notifications/notifications.js
+++ client/templates/notifications/notifications.js
@@ -8,5 +8,21 @@ Template.notifications.helpers({
 });
 
 Template.notifications.events({
+  'click #accept-button': function (e) {
+    var self = this;
 
+    var request = {
+      userId: self.userId,
+      requestingUserId: self.requestingUserId,
+      requestId: self.extraFields.requestId,
+      projectId: self.extraFields.projectId,
+      notificationId: self._id
+    };
+
+    Meteor.call('addUserToProject', request, function(err, result) {
+      if (err) {
+        console.log(err.reason);
+      }
+    });
+  }
 });
