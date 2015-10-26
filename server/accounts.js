b2c86412bdcdcadf8dd91059d0bda8eff4fd6407
Benny Zlotnik <uber442@gmail.com>
2015-10-26 14:44:45 +0200

    @matanshukry fixed the registeration

--- server/accounts.js
+++ server/accounts.js
@@ -6,6 +6,7 @@ Accounts.onCreateUser(function(option, user) {
   var hash = Gravatar.hash(user.emails[0].address);
   var gravatarURL = Gravatar.imageUrl(hash);
   _.extend(user.profile, { gravatarURL: gravatarURL});
-
+  _.extend(user.profile, { firstName: option.profile.firstName });
+  _.extend(user.profile, { lastName: option.profile.lastName });
   return user;
 });
